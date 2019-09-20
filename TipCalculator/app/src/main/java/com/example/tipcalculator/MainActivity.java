f67fd2d49800703c6d94f4f6514c98c37a06a4f1
John Anselmo <jjohnanselmo@gmail.com>
2019-09-20 12:20:41 -0400

    Physical layout and into the inside

--- TipCalculator/app/src/main/java/com/example/tipcalculator/MainActivity.java
+++ TipCalculator/app/src/main/java/com/example/tipcalculator/MainActivity.java
@@ -3,6 +3,12 @@ package com.example.tipcalculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Spinner tipSpinner = findViewById(R.id.tipSpinner);
+        ArrayAdapter<String> tipSpinnerArrayAdapter;
+        tipSpinnerArrayAdapter = new ArrayAdapter<String>(this, android.R.layout.tipSpinner, getResources().getStringArray(R.string.tipPercentages))//Arrays.asList("0.0", "5.0", "10.0", "15.0", "20.0"));
+        tipSpinner.setAdapter(tipSpinnerArrayAdapter);
     }
 }
