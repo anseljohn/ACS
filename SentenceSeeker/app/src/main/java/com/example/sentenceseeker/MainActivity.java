4f329d07c0c8d3e22db59b76e61e218aeade7b0c
John Anselmo <jjohnanselmo@gmail.com>
2019-09-24 11:34:35 -0400

    Basic seeker activity layout and fading logo

--- SentenceSeeker/app/src/main/java/com/example/sentenceseeker/MainActivity.java
+++ SentenceSeeker/app/src/main/java/com/example/sentenceseeker/MainActivity.java
@@ -2,13 +2,42 @@ package com.example.sentenceseeker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
+    private static int timeBetween = 3500;
+    private Handler mWaitHandler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mWaitHandler.postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+
+                try {
+
+                    Intent intent = new Intent(getApplicationContext(), SentenceSeeker.class);
+                    startActivity(intent);
+
+                    finish();
+                } catch (Exception ignored) {
+                    ignored.printStackTrace();
+                }
+            }
+        }, timeBetween);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+        //Remove all the callbacks otherwise navigation will execute even after activity is killed or closed.
+        mWaitHandler.removeCallbacksAndMessages(null);
     }
 }
