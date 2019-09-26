e9235ca9b58991aa4399773840cd386502d8b912
John Anselmo <jjohnanselmo@gmail.com>
2019-09-26 12:30:53 -0400

    Radiobuttons working

--- SpellingGame/app/src/main/java/com/example/spellinggame/Word.java
+++ SpellingGame/app/src/main/java/com/example/spellinggame/Word.java
@@ -1,7 +1,7 @@
 package com.example.spellinggame;
 
 public class Word {
-    public boolean spelledCorrectly
+    public boolean spelledCorrectly;
     public String word;
 
     public Word(boolean spelledCorrectly, String word) {
