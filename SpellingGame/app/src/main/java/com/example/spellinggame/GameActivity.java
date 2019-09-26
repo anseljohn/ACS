c0e2e342796faa0a7a1a45f4ca3ad825a5a3e16c
John Anselmo <jjohnanselmo@gmail.com>
2019-09-26 12:15:35 -0400

    Adding words

--- SpellingGame/app/src/main/java/com/example/spellinggame/GameActivity.java
+++ SpellingGame/app/src/main/java/com/example/spellinggame/GameActivity.java
@@ -8,9 +8,24 @@ import android.os.Handler;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+
 public class GameActivity extends AppCompatActivity {
 
     private TextView timerTextView;
+    private ArrayList<Word> words = new ArrayList<>();
+    private Word[] wordsArray = {new Word(false, "abcense"),
+                                 new Word(true, "absence"),
+                                 new Word(true, "intelligence"),
+                                 new Word(false, "inteligence"),
+                                 new Word(true, "immediately"),
+                                 new Word(false, "imediately"),
+                                 new Word(true, "privilege"),
+                                 new Word(false, "privelege"),
+                                 new Word(true, "underrate"),
+                                 new Word(false, "underate"),
+                                 new Word(true, "writing"),
+                                 new Word(false,"writeing"),};
 
     CountDownTimer timer;
 
@@ -19,6 +34,7 @@ public class GameActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
         timerTextView = findViewById(R.id.timerText);
+
 
         timer = new CountDownTimer(300000, 1000) {
             @Override
---
+++ SpellingGame/app/src/main/java/com/example/spellinggame/Word.java
@@ -0,0 +1,11 @@
+package com.example.spellinggame;
+
+public class Word {
+    public boolean spelledCorrectly
+    public String word;
+
+    public Word(boolean spelledCorrectly, String word) {
+        this.spelledCorrectly = spelledCorrectly;
+        this.word = word;
+    }
+}
