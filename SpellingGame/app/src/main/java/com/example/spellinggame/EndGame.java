3416e3513730cb725a51ced7071a1abd5bc46c8c
John Anselmo <jjohnanselmo@gmail.com>
2019-10-23 11:37:15 -0400

    Start endscreen

---
+++ SpellingGame/app/src/main/java/com/example/spellinggame/EndGame.java
@@ -0,0 +1,14 @@
+baspackage com.example.spellinggame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EndGame extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_end_game);
+    }
+}
