3e922e0bc4655b7f23044010647f11cd62bdff78
John Anselmo <jjohnanselmo@gmail.com>
2019-11-15 12:31:20 -0500

    Added generating random background colors

--- ColorGuesser/app/src/main/java/com/example/colorguesser/MainActivity.java
+++ ColorGuesser/app/src/main/java/com/example/colorguesser/MainActivity.java
@@ -2,13 +2,42 @@ package com.example.colorguesser;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.text.Layout;
+import android.view.View;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
+    private CountDownTimer timer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        generateBackground();
+    }
+
+    public void generateBackground() {
+        final View bg = findViewById(R.id.layout);
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                bg.setBackgroundColor(Color.parseColor(rgbToHex(getRandomColor())));
+            }
+        };
+    }
+
+    public int[] getRandomColor() {
+        Random random = new Random();
+        return new int[] {random.nextInt(255), random.nextInt(255), random.nextInt(255)};
+    }
+
+    public String rgbToHex(int[] rgb) {
+        return String.format("#%02x%02x%02x", rgb[0], rgb[1], rgb[2]);
     }
 }
